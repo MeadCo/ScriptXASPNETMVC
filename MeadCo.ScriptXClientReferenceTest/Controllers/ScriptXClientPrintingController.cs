439008c591f503cd5238e6347692ea68254cd724
Pete Cole <pcole@meadroid.com>
2016-05-27 18:50:04 +0100

    Missing using statement in package printcontroller template

--- MeadCo.ScriptXClientReferenceTest/Controllers/ScriptXClientPrintingController.cs
+++ MeadCo.ScriptXClientReferenceTest/Controllers/ScriptXClientPrintingController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using MeadCo.ScriptX;
-using MeadCo.ScriptXClient;
 
 namespace MeadCo.ScriptXClientReference.Controllers
 {
