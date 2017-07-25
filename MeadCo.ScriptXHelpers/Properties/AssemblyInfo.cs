afe6e117a2d53bf2af142f6aee93603e4ada8d18
Pete Cole <pcole@meadroid.com>
2017-07-25 17:11:42 +0100

    Build for .net framework 4.5

--- MeadCo.ScriptXHelpers/Properties/AssemblyInfo.cs
+++ MeadCo.ScriptXHelpers/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("2.1.0.0")]
 [assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0-beta01")]
+[assembly: AssemblyInformationalVersion("2.1.0-beta02")]
